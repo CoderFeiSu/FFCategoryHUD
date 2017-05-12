1d8d9a186cb5a344696ae8f5f6e04d0855329230
sf290798 <sf290798@163.com>
2017-05-12 21:44:15 +0800

    增加说明

--- FFCategoryHUDExample/FFCategoryHUDExample/ViewController.swift
+++ FFCategoryHUDExample/FFCategoryHUDExample/ViewController.swift
@@ -15,66 +15,66 @@ class ViewController: UIViewController {
  
         automaticallyAdjustsScrollViewInsets = false
         
-//        let frame = CGRect(x: 0, y: 64, width: 375, height: view.bounds.height - 64)
-//        let titles = ["哈哈df","哈哈哈sdfas","哈哈哈333","哈","134124哈哈哈","说就撒了开发商"]
-//        var barStyle = FFSegmentBarStyle()
-//        barStyle.isShowBottomLine = true
-//        barStyle.isNeedScale = true
-//        barStyle.bottomLineColor = UIColor.red
-//        var childVCs = [UIViewController]()
-//        for _ in 0..<titles.count {
-//            let vc = UIViewController()
-//            vc.view.backgroundColor = UIColor.random
-//            childVCs.append(vc)
-//        }
-//        let segmentContainer = FFSegmentContainer.init(frame: frame, barTitles: titles, barStyle: barStyle, childVCs: childVCs, parentVC: self)
-//        view.addSubview(segmentContainer)
-        
-        
-        let frame = CGRect(x: 0, y: view.bounds.height - 216, width: view.bounds.width, height: 216)
-        let titles = ["低级", "初级", "中级", "高级"]
-        var  barStyle = FFKeyboardBarStyle()
+        let frame = CGRect(x: 0, y: 64, width: 375, height: view.bounds.height - 64)
+        let titles = ["哈哈df","哈哈哈sdfas","哈哈哈333","哈","134124哈哈哈","说就撒了开发商"]
+        var barStyle = FFSegmentBarStyle()
         barStyle.isShowBottomLine = true
+        barStyle.isNeedScale = true
         barStyle.bottomLineColor = UIColor.red
-        let layout = FFCategoryKeyboardLayout()
-        let keyboardContainer = FFKeyboardContainer.init(frame: frame, barTitles: titles, barStyle: barStyle, layout: layout)
-        keyboardContainer.delegate = self
-        keyboardContainer.dataSource = self
-        keyboardContainer.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "abc")
-        view.addSubview(keyboardContainer)
-        
-        
-    }
-    
-    
-
-
-}
-
-
-
-extension ViewController: FFKeyboardContainerDataSource, FFKeyboardContainerDelegate {
-
-    func numberOfSections(in keyboardContainer: FFKeyboardContainer) -> Int {
-        return 4
-    }
-    
-    func keyboardContainer(_ keyboardContainer: FFKeyboardContainer, numberOfItemsInSection section: Int) -> Int {
-        if (section == 0) {
-        return 1
-        } else if (section == 1) {
-          return 50
-        } else if (section == 2) {
-            return 22
-        } else {
-          return 10
+        var childVCs = [UIViewController]()
+        for _ in 0..<titles.count {
+            let vc = UIViewController()
+            vc.view.backgroundColor = UIColor.random
+            childVCs.append(vc)
         }
+        let segmentContainer = FFSegmentContainer.init(frame: frame, barTitles: titles, barStyle: barStyle, childVCs: childVCs, parentVC: self)
+        view.addSubview(segmentContainer)
+        
+        
+//        let frame = CGRect(x: 0, y: view.bounds.height - 216, width: view.bounds.width, height: 216)
+//        let titles = ["低级", "初级", "中级", "高级"]
+//        var  barStyle = FFKeyboardBarStyle()
+//        barStyle.isShowBottomLine = true
+//        barStyle.bottomLineColor = UIColor.red
+//        let layout = FFCategoryKeyboardLayout()
+//        let keyboardContainer = FFKeyboardContainer.init(frame: frame, barTitles: titles, barStyle: barStyle, layout: layout)
+//        keyboardContainer.delegate = self
+//        keyboardContainer.dataSource = self
+//        keyboardContainer.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "abc")
+//        view.addSubview(keyboardContainer)
+        
+        
     }
     
-    func keyboardContainer(_ keyboardContainer: FFKeyboardContainer, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = keyboardContainer.dequeueReusableCell(withReuseIdentifier: "abc", for: indexPath)
-        cell.backgroundColor = UIColor.random
-        return cell
-    }
     
+
+
 }
+
+
+
+//extension ViewController: FFKeyboardContainerDataSource, FFKeyboardContainerDelegate {
+//
+//    func numberOfSections(in keyboardContainer: FFKeyboardContainer) -> Int {
+//        return 4
+//    }
+//    
+//    func keyboardContainer(_ keyboardContainer: FFKeyboardContainer, numberOfItemsInSection section: Int) -> Int {
+//        if (section == 0) {
+//        return 1
+//        } else if (section == 1) {
+//          return 50
+//        } else if (section == 2) {
+//            return 22
+//        } else {
+//          return 10
+//        }
+//    }
+//    
+//    func keyboardContainer(_ keyboardContainer: FFKeyboardContainer, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+//        let cell = keyboardContainer.dequeueReusableCell(withReuseIdentifier: "abc", for: indexPath)
+//        cell.backgroundColor = UIColor.random
+//        return cell
+//    }
+//    
+//}
--- README.md
+++ README.md
@@ -1,46 +1,32 @@
 ## FFCategoryHUd
-* 简单易用，向iOS系统看齐，会用UIAlertController就会用FFPopover
-* 让你的菜单想上弹就上弹，想下弹就下弹
+* 里面有两个框架，一个类似于网易新闻顶部的滚动条，一个是图片键盘
+* 简单易用
 
 ## 怎样使用
-* 手动拖入FFPopover文件夹到项目中
-* 导入主文件：#import "FFPopover.h"
+* 手动拖入FFCategoryHUD文件夹到项目中
 * 示例代码:
 
 ```objc
-// 右上边加号点击
-- (IBAction)rightPlusClicked:(UITapGestureRecognizer *)sender {
-    
-    
-    FFPopoverController *popover = [[FFPopoverController alloc] initWithFromView:sender.view ];
 
-    [popover addAction: [[FFPopoverAction alloc] initWithTitle:@"搜一搜" image:[UIImage imageNamed:@"search"] handler:^(FFPopoverAction *action) {
-        UIViewController *vc = [[UIViewController alloc] init];
-        vc.view.backgroundColor = [UIColor redColor];
-        vc.title = @"我要搜索";
-        [self.navigationController pushViewController:vc animated:YES];
-    }]];
-    
-    [popover addAction: [[FFPopoverAction alloc] initWithTitle:@"扫一扫" image:[UIImage imageNamed:@"QRCode"] handler:^(FFPopoverAction *action) {
-        UIViewController *vc = [[UIViewController alloc] init];
-        vc.view.backgroundColor = [UIColor greenColor];
-        vc.title = @"我要扫描";
-        [self.navigationController pushViewController:vc animated:YES];
-    }]];
-    
-    [popover addAction: [[FFPopoverAction alloc] initWithTitle:@"聊一聊" image:[UIImage imageNamed:@"chat"] handler:^(FFPopoverAction *action) {
-        UIViewController *vc = [[UIViewController alloc] init];
-        vc.view.backgroundColor = [UIColor blueColor];
-        vc.title = @"我要聊天";
-        [self.navigationController pushViewController:vc animated:YES];
-    }]];
-    
-    [self presentViewController:popover animated:YES completion:nil];
-}
+        let frame = CGRect(x: 0, y: 64, width: 375, height: view.bounds.height - 64)
+        let titles = ["哈哈df","哈哈哈sdfas","哈哈哈333","哈","134124哈哈哈","说就撒了开发商"]
+        var barStyle = FFSegmentBarStyle()
+        barStyle.isShowBottomLine = true
+        barStyle.isNeedScale = true
+        barStyle.bottomLineColor = UIColor.red
+        var childVCs = [UIViewController]()
+        for _ in 0..<titles.count {
+            let vc = UIViewController()
+            vc.view.backgroundColor = UIColor.random
+            childVCs.append(vc)
+        }
+        let segmentContainer = FFSegmentContainer.init(frame: frame, barTitles: titles, barStyle: barStyle, childVCs: childVCs, parentVC: self)
+        view.addSubview(segmentContainer)
+
 ```
 
 ## 效果图
-<img src="http://s4.sinaimg.cn/bmiddle/003uLCdEzy790B9kdLd73&amp;690" width="280" height="497" name="image_operate_71751487430741898" id="image_operate_59231487430974390">
+<img src="http://s15.sinaimg.cn/mw690/003uLCdEzy7b1mRO6aG6e&amp;690" width="279" height="491" id="image_operate_56541494594904791">
 
 ## 支持版本
 iOS7及以上
